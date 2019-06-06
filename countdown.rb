49f28b63ff1165e48237f1da2ae9d196288106b9
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 17:54:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,14 @@
 
 def countdown (num)
   while num >= 1
+    puts "#{num} SECOND(S)!"
+    num -= 1
+
+  end
+  return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep (num)
+  while num >= 1
     sleep(1)
     puts "#{num} SECOND(S)!"
     num -= 1
